eb0af4a182380b961b174ae1585461893734c83d
Vladislav Kishchenko <vlad5114@gmail.com>
2022-02-23 20:23:10 +0300

    update with .action

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander/esm.mjs';
+import genDiff from '../src/index.js';
 
 const program = new Command();
 
@@ -10,6 +11,9 @@ program
   .version('0.1.0', '-V, --version', 'output the version number')
   .helpOption('-h, --help', 'output usage information')
   .option('-f, --format <type>', 'output format')
-  .argument('<filepath1> <filepath2>');
+  .argument('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2));
+  });
 
 program.parse();
