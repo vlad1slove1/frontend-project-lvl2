10b0a97315167ffa6f20bf9bc1265b529ec4971b
Vladislav Kishchenko <vlad5114@gmail.com>
2022-02-27 22:10:25 +0300

    replace logic from bin to src/index.js

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import { program } from 'commander/esm.mjs';
+import genDiff from './genDiff.js';
+
+const diff = () => {
+  program
+    .name('gendiff')
+    .description('Compares two configuration files and shows a difference.')
+    .version('0.1.0', '-V, --version', 'output the version number')
+    .helpOption('-h, --help', 'output usage information')
+    .option('-f, --format <type>', 'output format')
+    .argument('<filepath1> <filepath2>')
+    .action((filepath1, filepath2) => {
+      console.log(genDiff(filepath1, filepath2));
+    });
+
+  program.parse();
+};
+
+export default diff;
