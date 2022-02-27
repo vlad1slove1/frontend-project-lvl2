e06b03a2e934b5f7f2a16f0d0574e713a38b6fd9
Vladislav Kishchenko <vlad5114@gmail.com>
2022-02-27 22:10:01 +0300

    remove logic from bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,19 +1,5 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander/esm.mjs';
-import genDiff from '../src/index.js';
+import diff from '../src/index.js';
 
-const program = new Command();
-
-program
-  .name('gendiff')
-  .description('Compares two configuration files and shows a difference.')
-  .version('0.1.0', '-V, --version', 'output the version number')
-  .helpOption('-h, --help', 'output usage information')
-  .option('-f, --format <type>', 'output format')
-  .argument('<filepath1> <filepath2>')
-  .action((filepath1, filepath2) => {
-    console.log(genDiff(filepath1, filepath2));
-  });
-
-program.parse();
+diff();
