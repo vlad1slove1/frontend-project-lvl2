14ca20b340323ff462d0f9ace26a93e45466d27c
Vladislav Kishchenko <vlad5114@gmail.com>
2022-02-28 22:03:36 +0300

    add genDiff.test.js

---
+++ __tests__/genDiff.test.js
@@ -0,0 +1,17 @@
+import { test, expect } from '@jest/globals';
+import { dirname, resolve } from 'path';
+import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
+import genDiff from '../src/genDiff.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => resolve(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
+
+const expectedOutput = readFile('flatOutput.txt');
+
+test('gediff test', () => {
+  expect(genDiff('file1.json', 'file2.json')).toEqual(expectedOutput);
+});
