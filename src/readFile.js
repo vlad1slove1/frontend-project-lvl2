add521a475afcc441ca4e938302548016935a7f9
Vladislav Kishchenko <vlad5114@gmail.com>
2022-03-06 19:36:21 +0300

    remove JSON.parse from readFile

--- src/readFile.js
+++ src/readFile.js
@@ -6,9 +6,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => resolve(__dirname, '..', '__fixtures__', filename);
-const readFile = (filename) => {
-  const file = readFileSync(getFixturePath(filename), 'utf-8');
-  return JSON.parse(file);
-};
+const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
 export default readFile;
