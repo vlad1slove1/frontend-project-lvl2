e3b1505efb09d13a4ee957e6e721c8b62dcfd263
Vladislav Kishchenko <vlad5114@gmail.com>
2022-03-19 14:54:00 +0300

    add default fromat 'stylish'

--- src/index.js
+++ src/index.js
@@ -7,10 +7,10 @@ const diff = () => {
     .description('Compares two configuration files and shows a difference.')
     .version('0.1.0', '-V, --version', 'output the version number')
     .helpOption('-h, --help', 'output usage information')
-    .option('-f, --format <type>', 'output format')
+    .option('-f, --format <type>', 'output format', 'stylish')
     .arguments('<filepath1> <filepath2>')
     .action((filepath1, filepath2) => {
-      console.log(genDiff(filepath1, filepath2));
+      console.log(genDiff(filepath1, filepath2, program.opts().format));
     });
 
   program.parse();
