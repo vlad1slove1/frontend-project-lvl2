630c9526eac9dd53db1124f0db4cbe87ebaa496e
Vladislav Kishchenko <vlad5114@gmail.com>
2022-03-20 20:01:37 +0300

    update with diffTree

--- src/genDiff.js
+++ src/genDiff.js
@@ -2,17 +2,18 @@ import { extname } from 'path';
 import readFile from './readFile.js';
 import parse from './parsers.js';
 import genTree from './genTree.js';
-import stylish from './formatters/stylish.js';
+import diffTree from './formatters/index.js';
 
-const genDiff = (filepath1, filepath2) => {
+const genDiff = (filepath1, filepath2, format = 'stylish') => {
   const readFile1 = readFile(filepath1);
   const readFile2 = readFile(filepath2);
+
   const file1 = parse(readFile1, extname(filepath1));
   const file2 = parse(readFile2, extname(filepath2));
 
   const tree = genTree(file1, file2);
 
-  return stylish(tree);
+  return diffTree(tree, format);
 };
 
 export default genDiff;
