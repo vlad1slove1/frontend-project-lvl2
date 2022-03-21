f08a6abaaecfbebd077c0e3cbbadb9b3a004c075
Vladislav Kishchenko <vlad5114@gmail.com>
2022-03-21 22:23:38 +0300

    refactoring tests

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -4,9 +4,11 @@ import genDiff from '../src/genDiff.js';
 
 const expectedStylishOutput = readFile('stylishOutput.txt');
 const expectedPlainOutput = readFile('plainOutput.txt');
+const expectedJsonOutput = readFile('jsonOutput.txt');
 
 test('gendiff stylish .json test', () => {
   expect(genDiff('file1.json', 'file2.json')).toEqual(expectedStylishOutput);
+  expect(() => genDiff('file1.xml', 'file2.json')).toThrow('Incorrect file format');
 });
 
 test('gendiff stylish .yml and .yaml test', () => {
@@ -19,4 +21,16 @@ test('gendiff plain .json test', () => {
 
 test('gendiff plain .yml and .yaml test', () => {
   expect(genDiff('file1.yml', 'file2.yaml', 'plain')).toEqual(expectedPlainOutput);
+});
+
+test('gendiff .json to JSON test', () => {
+  expect(genDiff('file1.json', 'file2.json', 'json')).toEqual(expectedJsonOutput);
+});
+
+test('gendiff .yml and .yaml to JSON test', () => {
+  expect(genDiff('file1.yml', 'file2.yaml', 'json')).toEqual(expectedJsonOutput);
+});
+
+test('format check at null', () => {
+  expect(genDiff('file1.json', 'file2.json', 'format')).toBeNull();
 });
