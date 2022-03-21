63396ab09e61e840b41f697b331645fdce1123c4
Vladislav Kishchenko <vlad5114@gmail.com>
2022-03-21 22:24:01 +0300

    add json.js

---
+++ src/formatters/json.js
@@ -0,0 +1,6 @@
+const json = (tree) => JSON.stringify(tree);
+
+export default json;
+
+// JSON.stringify() - this command turns the object into a JSON string
+// for subsequent sending to the server.
