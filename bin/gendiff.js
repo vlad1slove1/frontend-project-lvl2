1cf96ac1c277c3ea83514c255c869d1f96829789
Vladislav Kishchenko <vlad5114@gmail.com>
2022-03-27 23:14:07 +0300

    replace commaner logic to bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,16 @@
 #!/usr/bin/env node
+import { program } from 'commander/esm.mjs';
+import genDiff from '../src/index.js';
 
-import diff from '../src/index.js';
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.1.0', '-V, --version', 'output the version number')
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2, program.opts().format));
+  });
 
-diff();
+program.parse();
