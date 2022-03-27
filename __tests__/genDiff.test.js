84fd027150c2de05b1734c7be0c805248343d5d5
Vladislav Kishchenko <vlad5114@gmail.com>
2022-03-27 23:13:26 +0300

    fix to test.each / remove JSON test

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,36 +1,44 @@
 import { test, expect } from '@jest/globals';
-import readFile from '../src/readFile.js';
-import genDiff from '../src/genDiff.js';
+import { dirname, resolve } from 'path';
+import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
+import genDiff from '../src/index.js';
 
-const expectedStylishOutput = readFile('stylishOutput.txt');
-const expectedPlainOutput = readFile('plainOutput.txt');
-const expectedJsonOutput = readFile('jsonOutput.txt');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-test('gendiff stylish .json test', () => {
-  expect(genDiff('file1.json', 'file2.json')).toEqual(expectedStylishOutput);
-  expect(() => genDiff('file1.xml', 'file2.json')).toThrow('Incorrect file format');
-});
+const getFixturePath = (filename) => resolve(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
-test('gendiff stylish .yml and .yaml test', () => {
-  expect(genDiff('file1.yml', 'file2.yaml')).toEqual(expectedStylishOutput);
-});
+const tests = [
+  {
+    filepath1: 'file1.json', filepath2: 'file2.json', output: 'stylishOutput.txt', format: 'stylish',
+  },
+  {
+    filepath1: 'file1.yml', filepath2: 'file2.yaml', output: 'stylishOutput.txt', format: 'stylish',
+  },
+  {
+    filepath1: 'file1.json', filepath2: 'file2.json', output: 'plainOutput.txt', format: 'plain',
+  },
+  {
+    filepath1: 'file1.yml', filepath2: 'file2.yaml', output: 'plainOutput.txt', format: 'plain',
+  },
+];
 
-test('gendiff plain .json test', () => {
-  expect(genDiff('file1.json', 'file2.json', 'plain')).toEqual(expectedPlainOutput);
-});
-
-test('gendiff plain .yml and .yaml test', () => {
-  expect(genDiff('file1.yml', 'file2.yaml', 'plain')).toEqual(expectedPlainOutput);
-});
-
-test('gendiff .json to JSON test', () => {
-  expect(genDiff('file1.json', 'file2.json', 'json')).toEqual(expectedJsonOutput);
-});
-
-test('gendiff .yml and .yaml to JSON test', () => {
-  expect(genDiff('file1.yml', 'file2.yaml', 'json')).toEqual(expectedJsonOutput);
+test.each(tests)('gendiff stylish and plain tests', ({
+  filepath1, filepath2, output, format,
+}) => {
+  const file1 = getFixturePath(filepath1);
+  const file2 = getFixturePath(filepath2);
+  const expected = readFile(output);
+  const result = genDiff(file1, file2, format);
+  expect(result).toEqual(expected);
 });
 
 test('format check at null', () => {
   expect(genDiff('file1.json', 'file2.json', 'format')).toBeNull();
+});
+
+test('format corectness check', () => {
+  expect(() => genDiff('file1.xml', 'file2.json')).toThrow('Incorrect file format');
 });
