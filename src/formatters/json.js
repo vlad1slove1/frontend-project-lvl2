b9d367905a796873f78f714a16eed9d2b50401c2
Vladislav Kishchenko <vlad5114@gmail.com>
2022-04-12 21:56:28 +0300

    remove unnecessary commentation

--- src/formatters/json.js
+++ src/formatters/json.js
@@ -1,6 +1,3 @@
 const json = (tree, spacesCount = 2) => JSON.stringify(tree, null, ' '.repeat(spacesCount));
 
 export default json;
-
-// JSON.stringify() - this command turns the object into a JSON string
-// for subsequent sending to the server.
