020ef6298a67a4da585d778c2129b14d206e7905
Vladislav Kishchenko <vlad5114@gmail.com>
2022-04-15 20:36:52 +0300

    remove logic from 'src/utils/' to 'src/utils.js'

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,9 @@
 import { resolve } from 'path';
 import { readFileSync } from 'fs';
 
+const getFormat = (filename) => filename.split('.')[1];
+
 const getFixturePath = (filename) => resolve(process.cwd(), filename);
 const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
-export default readFile;
+export { getFormat, readFile };
--- src/utils/getFormat.js
+++
@@ -1,1 +0,0 @@
-export default (filename) => filename.split('.')[1];
