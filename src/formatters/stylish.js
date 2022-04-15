acca3c6ad8e87a625de9a075346bbb714378a313
Vladislav Kishchenko <vlad5114@gmail.com>
2022-04-15 20:39:06 +0300

    update common logic

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,4 +1,4 @@
-const indent = (depth, replacer = ' ', spacesCount = 2) => replacer.repeat(depth * (spacesCount + 2) - 2);
+const indent = (depth, replacer = ' ', spacesCount = 4) => replacer.repeat(depth * spacesCount - 2);
 
 const stringify = (value, depth = 1) => {
   if (!(value instanceof Object)) {
@@ -15,7 +15,7 @@ const stringify = (value, depth = 1) => {
   return `{\n${result.join('\n')}\n  ${indent(depth)}}`;
 };
 
-const stylish = (nodes) => {
+const stylish = (tree) => {
   const iter = (node, depth = 0) => {
     const {
       key,
@@ -28,31 +28,33 @@ const stylish = (nodes) => {
 
     switch (type) {
       case 'root': {
-        return `${children.map((child) => iter(child, depth + 1)).join('')}`;
+        const result = children.flatMap((child) => iter(child, depth + 1));
+        return `{\n${result.join('\n')}\n}`;
       }
       case 'nested': {
-        return `\n${indent(depth)}  ${key}: {${children.map((child) => iter(child, depth + 1)).join('')}\n${indent(depth)}  }`;
+        const result = children.flatMap((child) => iter(child, depth + 1));
+        return `${indent(depth)}  ${key}: {\n${result.join('\n')}\n${indent(depth)}  }`;
       }
       case 'deleted': {
-        return `\n${indent(depth)}- ${key}: ${stringify(value, depth)}`;
+        return `${indent(depth)}- ${key}: ${stringify(value, depth)}`;
       }
       case 'added': {
-        return `\n${indent(depth)}+ ${key}: ${stringify(value, depth)}`;
+        return `${indent(depth)}+ ${key}: ${stringify(value, depth)}`;
       }
       case 'changed': {
-        const removed = `\n${indent(depth)}- ${key}: ${stringify(value1, depth)}`;
-        const added = `\n${indent(depth)}+ ${key}: ${stringify(value2, depth)}`;
-        return `${removed}${added}`;
+        const removed = `${indent(depth)}- ${key}: ${stringify(value1, depth)}`;
+        const added = `${indent(depth)}+ ${key}: ${stringify(value2, depth)}`;
+        return `${removed}\n${added}`;
       }
       case 'unchanged': {
-        return `\n${indent(depth)}  ${key}: ${stringify(value, depth)}`;
+        return `${indent(depth)}  ${key}: ${stringify(value, depth)}`;
       }
       default:
-        return null;
+        throw new Error(`Unknown type: '${type}' of node!`);
     }
   };
 
-  return `{${iter(nodes)}\n}`;
+  return iter(tree);
 };
 
 export default stylish;
